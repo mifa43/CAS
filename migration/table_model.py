b926ae24eeec949042f95b2428f02ffb93019e4b
Mifa43 <mifa43kotez@gmail.com>
2023-06-24 00:51:04 +0200

    dodata nova pola u model

--- migration/table_model.py
+++ migration/table_model.py
@@ -5,18 +5,20 @@ from sqlalchemy.orm import relationship
 class Kontakti_sbb(Base):
     """Model tabele koja ce da se kreira u bazi"""
     
-    __tablename__= "registration"   #ime tabele
+    __tablename__= "kontakt"   #ime tabele
      
     #kolone u tabeli i njihove karakteristike
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     ime = Column("ime", String)
     prezime = Column("prezime", String)
     adresa = Column("adresa",String)
-    post_code = Column("post_code",String)
+    post_code = Column("post_code", Integer)
     grad = Column("grad",String)   
     telefon = Column("telefon",Integer)
     kampanja = Column("kampanja",String)   
-
+    table_name = Column("table_name",String)   
+    indentification_code = Column("indentification_code",Integer)   
+    is_printed = Column("is_printed",String)   
 
     def __str__(self):
           return self.ime
