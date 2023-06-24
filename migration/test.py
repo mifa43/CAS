633cedc199c3cf571a6e6911b297ae4e8f879559
Mifa43 <mifa43kotez@gmail.com>
2023-06-24 11:38:56 +0200

    AKO JE contact.is_printed == "False" to znaci da necemo da generismo pismo

--- migration/test.py
+++ migration/test.py
@@ -3,24 +3,33 @@ from crud import KontaktiDB
 
 db = get_db()
 
-usr = KontaktiDB(db).get_rows_by_number(100, grad_param="Mladenovac",kampanja_param=None)
+get_contacts = KontaktiDB(db).get_rows_by_number(10, grad_param="Sombor",kampanja_param=None)
 
-if usr["succes"] == False:
+if get_contacts["succes"] == False:
 
-    print(usr["message"])
+    print(get_contacts["message"])
 
 else:
 
-    for i in usr["result"]:
-        print([i.id,
-            i.ime, 
-            i.prezime, 
-            i.adresa, 
-            i.post_code, 
-            i.grad, 
-            i.telefon, 
-            i.kampanja,
-            i.table_name,
-            i.indentification_code,
-            i.is_printed
-            ])
+    for contact in get_contacts["result"]:
+
+        if contact.is_printed == "False":
+            
+            update = KontaktiDB(db).update_by_id(contact.id)
+
+            print([contact.id,
+                contact.ime, 
+                contact.prezime, 
+                contact.adresa, 
+                contact.post_code, 
+                contact.grad, 
+                contact.telefon, 
+                contact.kampanja,
+                contact.table_name,
+                contact.indentification_code,
+                contact.is_printed
+                ])
+        
+        else:
+
+            print(f"Person with id {contact.id} for grad: {contact.grad} is  alredy printed!")
